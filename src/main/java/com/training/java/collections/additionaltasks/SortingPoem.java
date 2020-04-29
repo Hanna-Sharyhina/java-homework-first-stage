6a1a471c9f0c430498d1deab9d5ee51f43c85d92
Hanna Sharyhina <hanna.konstantinovna@gmail.com>
2020-04-29 17:45:46 +0300

    remove incorect exception declarations from SortingPoem.java

--- src/main/java/com/training/java/collections/additionaltasks/SortingPoem.java
+++ src/main/java/com/training/java/collections/additionaltasks/SortingPoem.java
@@ -3,25 +3,23 @@ package com.training.java.collections.additionaltasks;
 import java.io.*;
 import java.util.*;
 import java.util.logging.Logger;
-import com.training.customexceptions.FalseInputException;
 
-public class SortingPoem {
+public class SortingPoem{
     static List<String> poem = new ArrayList<>();
-    static int choice = 0;
+    static String choice;
     private static final Logger LOGGER = Logger.getLogger(SortingPoem.class.getSimpleName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         readLinesFromFile();
         outPutGenuinePoem();
-        chooseSortingMethodByUserChoice();
         runSortingByChosenCriteria();
     }
 
-    public static void readLinesFromFile() {
+    public static void readLinesFromFile(){
         try (BufferedReader reader
-                     = new BufferedReader(new FileReader("src\\main\\resources\\AnnabelLeePoem.txt"))) {
+                     = new BufferedReader(new FileReader("src\\main\\resources\\AnnabelLeePoem.txt"))){
             reader.lines().forEach(poem::add);
-        } catch (IOException ex) {
+        } catch (IOException ex){
             ex.printStackTrace();
         }
     }
@@ -33,44 +31,32 @@ public class SortingPoem {
         System.out.println();
     }
 
-    public static void chooseSortingMethodByUserChoice() {
+    public static void chooseSortingMethodByUserChoice(){
         System.out.println("Выберите способ сортировки, где: ");
         System.out.println("1 - сортировка по длине строк. ");
         System.out.println("2 - сортировка строк по алфавиту. ");
         System.out.println("3 - запись строк в обратном порядке в другой файл. ");
         Scanner scanner = new Scanner(System.in);
-        try{
-        if (!scanner.hasNextInt()) {
-            throw new FalseInputException();
-        }
-            choice = scanner.nextInt();
-        } catch (FalseInputException e){
-            LOGGER.warning(String.valueOf(e));
-            chooseSortingMethodByUserChoice();
-        }
+        choice = scanner.next().trim();
     }
 
-    public static void runSortingByChosenCriteria() {
+    public static void runSortingByChosenCriteria(){
+        chooseSortingMethodByUserChoice();
         switch (choice) {
-            case 1:
+            case "1":
                 sortPoemByLineLength();
                 outPutSortedPoem();
                 break;
-            case 2:
+            case "2":
                 sortPoemByAlphabetOrder();
                 outPutSortedPoem();
                 break;
-            case 3:
+            case "3":
                 writeReverseOrderOfPoemInDifferentFile();
                 break;
             default:
-                try {
-                    throw new FalseInputException();
-                } catch (FalseInputException e) {
-                    LOGGER.warning(String.valueOf(e));
-                    chooseSortingMethodByUserChoice();
-                    runSortingByChosenCriteria();
-                }
+                LOGGER.warning("Ввод осуществлен неверно. Попробуйте повторить. ");
+                runSortingByChosenCriteria();
         }
     }
 
@@ -84,14 +70,13 @@ public class SortingPoem {
 
     public static void writeReverseOrderOfPoemInDifferentFile(){
         try(BufferedWriter writer =
-                    new BufferedWriter(new FileWriter("src\\src\\main\\resources\\AnnabelLeePoemWithReverseOrder.txt"))) {
-                for (int i = poem.size()-1; i>=0; i--){
+                    new BufferedWriter(new FileWriter("src\\src\\main\\resources\\AnnabelLeePoemWithReverseOrder.txt"))){
+            for (int i = poem.size()-1; i>=0; i--){
                 writer.write(poem.get(i) + "\n");
             }
-        }catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
-
     }
 
     public static void outPutSortedPoem(){
