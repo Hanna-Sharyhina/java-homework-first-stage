dd04df5ddd6b9a79922fabd323ed7a11aa74013d
Hanna Sharyhina <hanna.konstantinovna@gmail.com>
2020-04-29 18:06:48 +0300

    remove incorect exception declarations from SampleSongByCriteria.java

--- src/main/java/com/training/java/collections/maintask/tools/SampleSongsByCriteria.java
+++ src/main/java/com/training/java/collections/maintask/tools/SampleSongsByCriteria.java
@@ -2,8 +2,6 @@ package com.training.java.collections.maintask.tools;
 
 import com.training.java.collections.maintask.song.Song;
 import com.training.java.collections.maintask.song.genres.SongStyles;
-import com.training.customexceptions.FalseInputException;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -12,9 +10,10 @@ import java.util.logging.Logger;
 
 import static com.training.java.collections.maintask.tools.RecordingMusicCD.musicCD;
 
-public class SampleSongsByCriteria {
+public class SampleSongsByCriteria{
 
     private static final Logger LOGGER = Logger.getLogger(SampleSongsByCriteria.class.getSimpleName());
+    private static final String FALSE_INPUT = "Неверный ввод. Попробуйте повторить. ";
     private String chosenStyle;
     double totalDuration = 0;
 
@@ -28,30 +27,27 @@ public class SampleSongsByCriteria {
                 (int)Math.floor(totalDuration) + " минут " + (int)((totalDuration - Math.floor(totalDuration))*60) + " секунд");
     }
 
-    public void findSongsWithDurationInChosenTimeRange() {
+    public void findSongsWithDurationInChosenTimeRange(){
         double from;
         double to;
         List<Song> songsInChosenTimeRange = new ArrayList<>();
         System.out.println("Введите временной диапазон для поиска композиций: ");
         Scanner scan = new Scanner(System.in);
-        try {
-            if (scan.hasNextDouble()) {
-                from = scan.nextDouble();
-                to = scan.nextDouble();
-                for (Song song : musicCD) {
-                    if (song.getDuration() >= from && song.getDuration() <= to) {
-                        songsInChosenTimeRange.add(song);
-                    }
-                } System.out.println(songsInChosenTimeRange);
-                if (songsInChosenTimeRange.isEmpty()) {
-                    LOGGER.warning("Композиций в данном временном диапазоне не найдено! ");
-                    findSongsWithDurationInChosenTimeRange();
+        if (scan.hasNextDouble()){
+            from = scan.nextDouble();
+            to = scan.nextDouble();
+            for (Song song : musicCD){
+                if (song.getDuration() >= from && song.getDuration() <= to){
+                    songsInChosenTimeRange.add(song);
                 }
-            } else {
-                throw new FalseInputException();
             }
-        } catch (FalseInputException e) {
-            LOGGER.warning(String.valueOf(e));
+            System.out.println(songsInChosenTimeRange);
+            if (songsInChosenTimeRange.isEmpty()){
+                LOGGER.warning("Композиций в данном временном диапазоне не найдено! ");
+                findSongsWithDurationInChosenTimeRange();
+            }
+        } else{
+            LOGGER.warning(FALSE_INPUT);
             findSongsWithDurationInChosenTimeRange();
         }
     }
@@ -64,28 +60,23 @@ public class SampleSongsByCriteria {
     public void setChosenStyle() {
         System.out.println("Введите один из жанров для выбора композиций: ");
         System.out.println(SongStyles.POP +" - " + SongStyles.POP.getSongStyleName() + ", " +
-                SongStyles.ART + " - " + SongStyles.ART.getSongStyleName() + ", " +
-                SongStyles.FOLKLORE + " - " + SongStyles.FOLKLORE.getSongStyleName());
+                            SongStyles.ART + " - " + SongStyles.ART.getSongStyleName() + ", " +
+                            SongStyles.FOLKLORE + " - " + SongStyles.FOLKLORE.getSongStyleName());
         Scanner scanner = new Scanner(System.in);
         chosenStyle = scanner.next().trim();
     }
 
-    public void selectSongsByUserChoice() {
+    public void selectSongsByUserChoice(){
         List<Song> chosenForListeningSongs = new ArrayList<>();
-
-        try {
-            for (Song song : musicCD) {
-                if (chosenStyle.equalsIgnoreCase(String.valueOf(song.getStyle())) ||
-                        chosenStyle.equalsIgnoreCase(song.getStyle().getSongStyleName())) {
-                    chosenForListeningSongs.add(song);
-                }
+        for (Song song : musicCD){
+            if (chosenStyle.equalsIgnoreCase(String.valueOf(song.getStyle())) ||
+                    chosenStyle.equalsIgnoreCase(song.getStyle().getSongStyleName())){
+                chosenForListeningSongs.add(song);
             }
-            System.out.println(chosenForListeningSongs);
-            if (chosenForListeningSongs.isEmpty()) {
-                throw new FalseInputException();
-            }
-        } catch (FalseInputException e) {
-            LOGGER.warning(String.valueOf(e));
+        }
+        System.out.println(chosenForListeningSongs);
+        if (chosenForListeningSongs.isEmpty()){
+            LOGGER.warning(FALSE_INPUT);
             setChosenStyle();
             selectSongsByUserChoice();
         }
