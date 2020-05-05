7e0631e6e54c8b3c3cc8583d9e9127a5ad62a9a1
Hanna Sharyhina <hanna.konstantinovna@gmail.com>
2020-05-05 18:42:57 +0300

    remove unused directory

--- src/main/java/com/training/customexceptions/FalseInputException.java
+++
@@ -1,8 +0,0 @@
-package com.training.customexceptions;
-
-public class FalseInputException extends Exception {
-    @Override
-    public String toString(){
-        return "Ввод осуществлен неверно, попробуйте повторить.";
-    }
-}
