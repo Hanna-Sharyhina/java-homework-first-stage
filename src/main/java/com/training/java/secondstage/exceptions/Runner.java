7d52f96d907b785b00d2c0d9cb87dbddd3a1dbcb
Hanna Sharyhina <hanna.konstantinovna@gmail.com>
2020-05-18 15:51:43 +0300

    add Runner class to Exceptions project

---
+++ src/main/java/com/training/java/secondstage/exceptions/Runner.java
@@ -0,0 +1,8 @@
+package com.training.java.secondstage.exceptions;
+
+public class Runner {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.runByUserChoice();
+    }
+}
